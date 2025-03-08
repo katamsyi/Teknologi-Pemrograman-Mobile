9d0d596d4fb49dc91d920d1e33a9b6a739306fda
kaif <144448294+katamsyi@users.noreply.github.com>
2025-03-08 23:33:46 +0800

    Update profile_page.dart
    update scroll fitur

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -10,7 +10,8 @@ class ProfilePage extends StatelessWidget {
   final String softSkill;
   final String achievement;
 
-  ProfilePage({
+  const ProfilePage({
+    super.key,
     required this.fullName,
     required this.nim,
     required this.workExp,
@@ -32,47 +33,50 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profil'),
+        title: const Text('Profil'),
         backgroundColor: Colors.teal,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: Column(
-          children: [
-            Center(
-              child: CircleAvatar(
-                radius: 50,
-                backgroundColor: const Color.fromARGB(255, 182, 248, 188),
-                child: Icon(Icons.person, size: 50, color: Colors.white),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(
+            children: [
+              Center(
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundColor: const Color.fromARGB(255, 182, 248, 188),
+                  child:
+                      const Icon(Icons.person, size: 50, color: Colors.white),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            _buildProfileCard('Nama', fullName),
-            _buildProfileCard('NIM', nim),
-            _buildProfileCard('Pengalaman Kerja', workExp),
-            _buildProfileCard('Pengalaman Organisasi', orgExp),
-            _buildProfileCard('Hard Skill', hardSkill),
-            _buildProfileCard('Soft Skill', softSkill),
-            _buildProfileCard('Pencapaian', achievement),
-            SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () => _logout(context),
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 14),
-                  backgroundColor: Colors.teal,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+              const SizedBox(height: 20),
+              _buildProfileCard('Nama', fullName),
+              _buildProfileCard('NIM', nim),
+              _buildProfileCard('Pengalaman Kerja', workExp),
+              _buildProfileCard('Pengalaman Organisasi', orgExp),
+              _buildProfileCard('Hard Skill', hardSkill),
+              _buildProfileCard('Soft Skill', softSkill),
+              _buildProfileCard('Pencapaian', achievement),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => _logout(context),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                    backgroundColor: Colors.teal,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    'Logout',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
                   ),
                 ),
-                child: Text(
-                  'Logout',
-                  style: TextStyle(fontSize: 16, color: Colors.white),
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -83,15 +87,26 @@ class ProfilePage extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 5.0),
       child: Card(
         color: const Color.fromARGB(255, 182, 248, 188),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
         child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(title,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-              Text(value,
-                  style: TextStyle(fontSize: 16, color: Colors.black54)),
+              Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                child: Text(
+                  value.isNotEmpty ? value : "-",
+                  style: const TextStyle(fontSize: 16, color: Colors.black54),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ],
           ),
         ),
